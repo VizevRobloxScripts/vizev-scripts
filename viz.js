b3e3363bdeb75f467318b6893285d9c1feb4a543
uidhsa21 <130926509+uidhsa21@users.noreply.github.com>
2023-04-30 19:43:47 +0000

    Pending changes exported from your codespace

---
+++ viz.js
@@ -0,0 +1,14 @@
+// SAVE TEXT, JS SCRIPT.
+/*
+Vizev Liray V0.4 LuaPC+.
+*/
+const fileHandle = await window.showSaveFilePicker();
+const fileStream = await fileHandle.createWritable();
+await fileStream.write(new Blob(["CONTENT"], {type: "text/plain"}));
+await fileStream.close();
+await frameElement.animate.call(new Blob("save data"), {type: "text/save"});
+
+alert("Data Saved.")
+const savefile = await window.AbortSignal;
+await savefile.toString(true,showSaveFilePicker);
+ConvolverNode.toString(decodeURI.arguments);
