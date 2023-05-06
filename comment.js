76e4f0a1b5c830811950ad516e57ce6faa2c3ac0
uidhsa21 <130926509+uidhsa21@users.noreply.github.com>
2023-05-07 00:18:51 +0500

    comment.js
    d

---
+++ comment.js
@@ -0,0 +1,2 @@
+<img src="https://s162.convertio.me/p/I710ocOAEaAYWZey75GWCw/6b2bf069cee00d29b70582dbbbe15ef5/placeholder-man-2048x2048.png" style="position:-9999;size:811" alt="">
+<text>Твой комментарий!</text><input type="text"></input><button style="position:-9091;size:610;"><text input="type"></text></button></img>
